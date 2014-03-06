a97732e727c07ae903652200530f417d6769b379
NoBodyKnowsTheTroubleIHaveSeen <whhwkm100@163.com>
2014-03-06 20:49:11 +0800

    update at 3.5

--- db/schema.rb
+++ db/schema.rb
@@ -13,52 +13,113 @@
 
 ActiveRecord::Schema.define(version: 20140117114130) do
 
+  create_table "answers", force: true do |t|
+    t.integer "people_id",   null: false
+    t.string  "content",     null: false
+    t.date    "answer_date", null: false
+  end
+
+  add_index "answers", ["people_id"], name: "fk1", using: :btree
+
   create_table "people", force: true do |t|
     t.integer "no"
-    t.string  "name",                    null: false
-    t.string  "gender",       limit: 10, null: false
-    t.integer "departmentId"
-    t.integer "projectId",               null: false
-    t.string  "password",                null: false
-    t.string  "email",                   null: false
+    t.string  "name",                     null: false
+    t.string  "gender",        limit: 10, null: false
+    t.integer "department_id"
+    t.string  "password",                 null: false
+    t.string  "email",                    null: false
   end
 
   create_table "person_projects", force: true do |t|
-    t.integer "projectId",           null: false
-    t.integer "peopleId",            null: false
-    t.integer "role",      limit: 1, null: false
-    t.string  "roleName",            null: false
+    t.integer "project_id",           null: false
+    t.integer "people_id",            null: false
+    t.integer "role",       limit: 1, null: false
+    t.string  "role_name",            null: false
   end
+
+  add_index "person_projects", ["people_id"], name: "fk2", using: :btree
+  add_index "person_projects", ["project_id"], name: "fk3", using: :btree
+
+  create_table "person_tasks", force: true do |t|
+    t.integer "people_id",             null: false
+    t.integer "project_id",            null: false
+    t.integer "task_id",               null: false
+    t.integer "hard_level",  limit: 1, null: false
+    t.date    "start_time",            null: false
+    t.date    "finish_time"
+    t.integer "is_doned",    limit: 1, null: false
+  end
+
+  add_index "person_tasks", ["people_id"], name: "fk4", using: :btree
+  add_index "person_tasks", ["project_id"], name: "fk5", using: :btree
+
+  create_table "plans", force: true do |t|
+    t.integer "people_id",             null: false
+    t.integer "task_id"
+    t.string  "name"
+    t.string  "description"
+    t.integer "hard_level",  limit: 1, null: false
+    t.date    "create_date",           null: false
+    t.date    "start_date"
+    t.date    "finish_date"
+  end
+
+  add_index "plans", ["people_id"], name: "fk6", using: :btree
+  add_index "plans", ["task_id"], name: "fk7", using: :btree
 
   create_table "projects", force: true do |t|
-    t.string  "name",                     null: false
-    t.string  "description", limit: 1000
-    t.date    "startDate",                null: false
-    t.integer "managerId",                null: false
+    t.string  "name",                           null: false
+    t.string  "description",       limit: 1000
+    t.date    "start_date",                     null: false
+    t.date    "finish_date"
+    t.integer "finish_percentage", limit: 1
+    t.integer "manager_id",                     null: false
+    t.integer "is_doned",          limit: 1,    null: false
   end
+
+  add_index "projects", ["manager_id"], name: "fk8", using: :btree
+
+  create_table "questions", force: true do |t|
+    t.integer "people_id",   null: false
+    t.string  "title",       null: false
+    t.string  "content",     null: false
+    t.date    "create_date", null: false
+  end
+
+  add_index "questions", ["people_id"], name: "fk9", using: :btree
+
+  create_table "schedule", force: true do |t|
+    t.integer "people_id",           null: false
+    t.integer "task_id"
+    t.integer "plan_id"
+    t.date    "date"
+    t.integer "complete_percentage", null: false
+    t.string  "description"
+    t.string  "content",             null: false
+  end
+
+  add_index "schedule", ["people_id"], name: "fk10", using: :btree
+  add_index "schedule", ["plan_id"], name: "fk12", using: :btree
+  add_index "schedule", ["task_id"], name: "fk11", using: :btree
 
   create_table "shares", force: true do |t|
-    t.integer "peopleId",  null: false
-    t.date    "shareDate", null: false
-    t.string  "shareURL",  null: false
+    t.integer "people_id",           null: false
+    t.string  "name",      limit: 0, null: false
+    t.string  "path",                null: false
   end
 
-  create_table "task_details", force: true do |t|
-    t.integer "taskId",    null: false
-    t.string  "question"
-    t.string  "work"
-    t.string  "scheduled"
-    t.date    "startDate"
-  end
+  add_index "shares", ["people_id"], name: "fk13", using: :btree
 
   create_table "tasks", force: true do |t|
-    t.integer "peopleId",                 null: false
+    t.integer "project_id",               null: false
     t.string  "name",                     null: false
     t.string  "description", limit: 1000
     t.integer "percentage",  limit: 1,    null: false
     t.integer "weight",      limit: 1,    null: false
-    t.date    "startDate",                null: false
-    t.date    "endDate"
+    t.date    "start_date",               null: false
+    t.date    "end_date"
   end
+
+  add_index "tasks", ["project_id"], name: "fk", using: :btree
 
 end
