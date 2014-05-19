a1f258fec1d38b382c8d8a4a836cc3479b301757
zyd232 <zyd232@gmail.com>
2014-05-19 12:32:36 +0800

    update at 3.20

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,8 +1,14 @@
 $(function () {
     $(".registerButton").click(function () {
-        $(".registerDiv").fadeIn("slow");
+        var div1=$(".loginDiv");
+        div1.animate({left:'100px',opacity:'0'});
+        var div2=$(".registerDiv");
+        div2.fadeIn("slow");
+
     })
     $(".closeButton").click(function () {
+        var div1=$(".loginDiv");
+        div1.animate({left:'-250',opacity:'1'});
         parent.hideIframe();
     })
     $("#registerForm").submit(function(){
