442aba0763cb4a30c3123c8629640a602e3842d7
NoBodyKnowsTheTroubleIHaveSeen <whhwkm100@163.com>
2014-05-20 20:27:43 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/assets/javascripts/task.js

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,39 +1,38 @@
 $(function () {
     $(".registerButton").click(function () {
-        var div1=$(".loginDiv");
-        div1.animate({left:'100px',opacity:'0'});
-        var div2=$(".registerDiv");
+        var div1 = $(".loginDiv");
+        div1.animate({left: '100px', opacity: '0'});
+        var div2 = $(".registerDiv");
         div2.fadeIn("slow");
 
     })
     $(".closeButton").click(function () {
-        var div1=$(".loginDiv");
-        div1.animate({left:'-250',opacity:'1'});
+        var div1 = $(".loginDiv");
+        div1.animate({left: '-250', opacity: '1'});
         parent.hideIframe();
     })
-    $("#loginForm").submit(function(){
+    $("#loginForm").submit(function () {
         formSubmit($(this));
     })
-    $("#registerForm").submit(function(){
+    $("#registerForm").submit(function () {
         formSubmit($(this));
     })
-    function formSubmit(object){
+    function formSubmit(object) {
         event.preventDefault();
         var url = object.attr("action");
         var data = object.serialize();
         var posting = $.post(url, data);
         posting.done(function (data) {
-            if("success"==data){
+            if ("success" == data) {
                 window.location = "../"
-            }else
-            {
-                $("#message").empty().append(data);
-                $("#message").fadeIn();
-                $("#message").fadeOut(3000);
+            } else {
+                $(".message").empty().append(data);
+                $(".message").fadeIn();
+                $(".message").fadeOut(3000);
             }
         });
     }
 })
-function hideIframe(){
+function hideIframe() {
     $(".registerDiv").fadeOut();
 }
